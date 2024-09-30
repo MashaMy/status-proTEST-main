b20afa5783cbcd6bf9b87e9f1eba0cd009841663
MashaMy <masha_myh@mail.ru>
2024-09-30 17:48:22 +0300

    your commit

--- src/components/Otziv.js
+++ src/components/Otziv.js
@@ -28,7 +28,7 @@ export const Otziv = () => (
 <Container class="col-6 center ">
     <h4 class='text-center pt-4 py-2'>Подкаст#1 от Екатерины Великой "Искусство создавать успех"</h4>
     <div class="ratio ratio-16x9">
-          <iframe width="750" height="650" src="https://rutube.ru/play/embed/d806f363c6d2c657625b5225f97ab5d5/?skinColor=000000" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen title="This is a unique title"></iframe>
+          <iframe width="750" height="650" src="https://rutube.ru/play/embed/d806f363c6d2c657625b5225f97ab5d5/?skinColor=000000" frameBorder="0" allow="clipboard-write; autoplay" webkitAllowFullScreen mozallowfullscreen allowFullScreen title="Is a unique title" ></iframe>
     </div>
     <div class = "text-center pt-4 py-5">
        <button type="button" className ="btn btn-outline-light"><a  href="https://rutube.ru/channel/37498849/" target="_blank "> Другие выпуски смотрите на канале RUTUBE</a></button>
